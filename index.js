f97d198116702b200285087e4b87aa45bd74cc9a
Andrew Kang <andrew716@gmail.com>
2018-05-27 03:52:46 -0700

    .

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const fs = require('fs')
 const net = require('net')
 const hat = require('hat')
 const path = require('path')
+const bncode = require('bncode')
 const parseTorrent = require('parse-torrent')
 const Discovery = require('torrent-discovery')
 const Protocol = require('bittorrent-protocol')
 const ut_metadata = require('ut_metadata')
 
-const torrent = parseTorrent('magnet:?xt=urn:btih:' + process.argv[2])
-const peerId = Buffer.from('-TR1330-' + hat(48))
+let infohash = process.argv[2]
 
-const MAX = 100
+if (!/^magnet:/.test(infohash)) {
+  infohash = 'magnet:?xt=urn:btih:' + infohash
+}
+
+const torrent = parseTorrent(infohash)
+const peerId = Buffer.from('-TR1330-' + hat(48))
+// console.log(torrent)
+
+const MAXPEER = 100
+const MAXCONN = 10
+
 let peers = []
 let queue = {}
 let count = 0
+let noPeerFound = setTimeout(() => {
+  process.exit()
+}, 5000)
 
 const discovery = new Discovery({
   infoHash: torrent.infoHashBuffer,
@@ -25,64 +38,65 @@ const discovery = new Discovery({
 })
 
 discovery.on('peer', peer => {
-  console.log('++peer++')
+  peers.push(peer)
 
-  if (!peers.includes(peer)) {
-    peers.push(peer)
+  if (noPeerFound) {
+    clearTimeout(noPeerFound)
+    noPeerFound = null
   }
 
-  if (peers.length >= MAX) {
+  if (peers.length >= MAXPEER) {
     discovery.destroy()
+  }
 
-    for (let i=0; i<10; i++) {
-      getMetadata()
-    }
+  if (count < MAXCONN) {
+    getMetadata()
   }
 })
 
-let done = false
 function save(metadata) {
-  const target = path.join(__dirname, 'storage', torrent.infoHash + '.torrent')
-  fs.writeFile(target, metadata, err => {
-    if (err) throw err
-    console.log(target)
+  const decoded = bncode.decode(metadata)
+  const info = decoded.info || {}
+  const name = info.name.toString()
+  const length = info.length
+  const files = (info.files || []).map(file => {
+    return { ...file, path: file.path.toString() };
   })
+
+  const target = path.join(__dirname, 'storage', name + '.torrent')
+  fs.writeFileSync(target, metadata)
+  console.log(JSON.stringify({ name, length, files }))
 }
 
 function getMetadata() {
-  console.log('peer', peers.length, count)
-
-  const peer = peers[count++]
+  const peer = peers.pop()
   const parts = peer.split(':')
   const socket = net.connect(parts[1], parts[0])
-  socket.id = peer
-  queue[peer] = { socket }
+  const wire = new Protocol()
 
-  socket.setTimeout(5000, () => socket.destroy());
+  // console.log('connect - peer', peer, 'peerlen', peers.length, 'count', ++count)
+  socket.id = peer
+  queue[peer] = { socket, wire }
+
+  wire.use(ut_metadata())
+  wire.ut_metadata.fetch()
+  wire.ut_metadata.on('metadata', metadata => {
+    // console.log('++ metadata ++')
+    peers = []
+
+    for (let key in queue) {
+      if (queue[key].wire) {
+        queue[key].wire.destroy()
+      }
+      queue[key].socket.destroy()
+    }
+
+    save(metadata)
+  })
+
+  socket.setTimeout(2000, () => socket.destroy());
   socket.on('connect', () => {
     socket.setTimeout(0)
-
-    const wire = new Protocol()
-    queue[socket.id].wire = wire
-
-    wire.use(ut_metadata())
-    wire.ut_metadata.fetch()
-    wire.ut_metadata.on('metadata', metadata => {
-      console.log('++ metadata ++')
-
-      done = true
-      peers = []
-      save(metadata)
-
-      for (let key in queue) {
-        console.log('+++++ force +++++', key)
-        if (queue[key].wire) {
-          queue[key].wire.destroy()
-        }
-        queue[key].socket.destroy()
-      }
-    })
-
     wire.handshake(torrent.infoHashBuffer, peerId)
     socket.pipe(wire).pipe(socket)
   });
@@ -91,19 +105,18 @@ function getMetadata() {
   socket.on('end', () => socket.destroy())
   socket.on('error', () => socket.destroy())
   socket.on('close', () => {
-    if (queue[socket.id].wire) {
+    if (queue[socket.id] && queue[socket.id].wire) {
       queue[socket.id].wire.destroy()
     }
     delete queue[socket.id]
     const queueLen = Object.keys(queue).length
     // console.log('++ closed ++', peer, 'count', count, 'queue', queueLen, 'peers', peers.length)
 
-    if (!done && count < peers.length) {
+    if (peers.length > 0) {
       getMetadata()
     }
 
     if (queueLen === 0) {
-      console.log('end')
       process.exit()
     }
   })
