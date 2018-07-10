168ec5d808dbfe7bf8d54a5cefaf7849c28f2e4c
Andrew Kang <andrew716@gmail.com>
2018-07-09 23:22:50 -0700

    add arguments

--- index.js
+++ index.js
@@ -21,33 +21,29 @@ let peerNotFound = setTimeout(() => {
   process.exit()
 }, 5000)
 
-try {
-  const pool = new PeerPool(torrent.infoHashBuffer, peerId)
-  const discovery = new Discovery({
-    infoHash: torrent.infoHashBuffer,
-    peerId,
-    dht: true,
-    tracker: false,
-    port: 6881,
-    announce: []
-  })
+const pool = new PeerPool(torrent.infoHashBuffer, peerId)
+const discovery = new Discovery({
+  infoHash: torrent.infoHashBuffer,
+  peerId,
+  dht: true,
+  tracker: false,
+  port: 6881,
+  announce: []
+})
 
-  discovery.on('peer', peer => {
-    log.debug('[peer]', peer)
+discovery.on('peer', peer => {
+  log.debug('[peer]', peer)
 
-    if (pool.push(peer)) {
-      pool.connect()
-    }
+  if (pool.push(peer)) {
+    pool.connect()
+  }
 
-    if (peerNotFound) {
-      clearTimeout(peerNotFound)
-      peerNotFound = null
-    }
+  if (peerNotFound) {
+    clearTimeout(peerNotFound)
+    peerNotFound = null
+  }
 
-    if (pool.len() >= MAX) {
-      discovery.destroy()
-    }
-  })
-} catch (e) {
-  log.error(e)
-}
+  if (pool.len() >= MAX) {
+    discovery.destroy()
+  }
+})
